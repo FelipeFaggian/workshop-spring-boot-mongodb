f46f273a7c502021d39b9293194f9f9b661e1009
faggian <felipesantosfaggian@gmail.com>
2023-06-04 12:40:06 -0300

    Consulta simples com query methods

--- src/main/java/com/felipefaggian/workshopmongo/services/PostService.java
+++ src/main/java/com/felipefaggian/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.felipefaggian.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipefaggian.workshopmongo.domain.Post;
-import com.felipefaggian.workshopmongo.domain.User;
 import com.felipefaggian.workshopmongo.repository.PostRepository;
 import com.felipefaggian.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -33,5 +33,9 @@ public class PostService {
 		}
 
 		return newPost;
+	}
+	
+	public List<Post> finByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
